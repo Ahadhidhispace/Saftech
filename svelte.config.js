261e1da45bffe41f3561f8d561d463b5266e1011
Amani Amani Jonanathan <ahadhiamanijonathan@gmail.com>
2023-06-10 08:28:56 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,7 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	checkOrigin: false,
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
